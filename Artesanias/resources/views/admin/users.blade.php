161ed27674c29a474a84063d96863dd8171b52e1
aragonec <47608292+aragonec@users.noreply.github.com>
2021-06-09 18:10:34 -0600

    Sistema de plantillas Blade

---
+++ Artesanias/resources/views/admin/users.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+    <h2>Estoy en USUARIOS</h2>
+@endsection
+
+@section('scripts')
+<script>console.log("este es un script")</script>
+@endsection
--- Artesanias/routes/web.php
+++ Artesanias/routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {return view('index');});
+Route::get('/admin', function () {return view('admin.index');});
+Route::get('/admin/usuarios', function () {return view('admin.users');});
+Route::get('/admin/productos', function () {return view('admin.productos');});
+
 Route::get('/practica', function () {return view('practica');});
 Route::get('/producto/{id}', function ($id) {
     return view('verproducto')->with('id',$id);
